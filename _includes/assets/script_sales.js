e3d9a2ab69258516d76c1c737c9441aa5e3420e7
Madam Ella <madamellamalala@gmail.com>
2025-11-07 14:19:05 +0200

    ,

--- _includes/assets/script_sales.js
+++ _includes/assets/script_sales.js
@@ -1,10 +1,11 @@
 /*** DATA ***/
-var tempText = "";
+var tempText = "", tempClassName = "";
 var vPageHistorique = "";
 
 var vSlug = document.querySelector("main").getAttribute("attr-slug");
 
-var tagContainer4historique = document.querySelector(".js-historique");
+var tagContainer4historique = document.querySelector(".js-records"),
+	tagMain = document.querySelector("main");
 /*** END DATA ***/
 
 /*** SETUP ***/
@@ -31,10 +32,10 @@ async function getHistorique() {
 				if(iContent[1] == "red") { tempText = "&cross;"; }
 				if(iContent[1] == "gray") { tempText = "&uarr;"; }
 
-				newTag4historique.innerHTML += "<div class='historique-list-item is-"+iContent[1]+"'><div><span>"+iContent[0]+"</span><span>"+iDate+"</span></div><div><span>"+tempText+"</span></div></div>";
+				newTag4historique.innerHTML += "<div class='records-list-item is-"+iContent[1]+"'><div class='records-list-content'><span>"+iContent[0]+"</span><span>"+iDate+"</span></div><div><span class='records-list-icon'>"+tempText+"</span></div></div>";
 				tempText = "";
 			}
-			newTag4historique.innerHTML += "<span>View Full Historique?</span>";
+			newTag4historique.innerHTML += "<span>View Full Records?</span>";
 			tagContainer4historique.appendChild(newTag4historique);
 		})
 		.catch(error => {
@@ -42,6 +43,18 @@ async function getHistorique() {
 		});
 		
 //	tagContainer4historique.scrollTop = tagContainer4historique.scrollHeight;
+}
+
+function doNav(theTag) {
+	tempClassName = theTag.classList;
+	
+	tagMain.classList.remove("is-dashboard");
+	tagMain.classList.remove("is-catalog");
+	tagMain.classList.remove("is-payslips");
+	
+	if(tempClassName.contains("is-dashboard")) { theTag.classList.add("is-dashboard"); tagMain.classList.add("is-dashboard"); }
+	else if(tempClassName.contains("is-catalog")) { theTag.classList.add("is-catalog"); tagMain.classList.add("is-catalog"); }
+	else if(tempClassName.contains("is-payslips")) { theTag.classList.add("is-payslips"); tagMain.classList.add("is-payslips"); }
 }
 /*** END FUNCTIONS ***/
 
